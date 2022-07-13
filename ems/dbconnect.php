6f492b892b86e5cfdca1a8fe5df52757d38f7966
t-kamoi <tk211014@gmail.com>
2022-07-13 16:48:27 +0900

    db modified

--- ems/dbconnect.php
+++ ems/dbconnect.php
@@ -1,13 +1,6 @@
 <?php
-		$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-		$server = $url["host"];
-		$username = $url["user"];
-		$password = $url["pass"];
-		$db = substr($url["path"], 1);
-
 	try{
-		$conn = new PDO($server, $username, $password, $db);
-		$conn -> set_charset('utf8');
+		$db=new PDO('mysql:dbname=heroku_c5e50f79a7ca84c;host=us-cdbr-east-05.cleardb.net;charset=utf-8','b6a593ac39066f','019b21d7');
 	} catch(PDOException $e){
 		echo 'DB接続エラー：' . $e -> getMessage();
 	}
