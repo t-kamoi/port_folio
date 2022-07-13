ed1a2b9d245c966968dd60dff4b9754539f81b3f
t-kamoi <tk211014@gmail.com>
2022-07-13 12:02:12 +0900

    db modified

--- ems/dbconnect.php
+++ ems/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$db=new PDO('mysql:dbname=heroku_f7234f0c90c463f;host=us-cdbr-east-05.cleardb.net;cherset=utf-8','b0469157d54e5e','f378ace7');
+		$db=new PDO('mysql:dbname=heroku_c5e50f79a7ca84c;host=us-cdbr-east-05.cleardb.net;cherset=utf-8','b6a593ac39066f','019b21d7');
 	} catch(PDOException $e){
 		echo 'DB接続エラー：' . $e -> getMessage();
 	}
